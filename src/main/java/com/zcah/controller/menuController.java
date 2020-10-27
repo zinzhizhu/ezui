38942599043f0e6426c53931a9cb3e70157b53c9
renhao <rh_pro@163.com>
2020-10-27 10:44:43 +0800

    11

--- src/main/java/com/zcah/controller/menuController.java
+++ src/main/java/com/zcah/controller/menuController.java
@@ -18,7 +18,7 @@ public class menuController {
 	@RequestMapping("/menu")
 	@ResponseBody
 	public List<Menu> selMenu() {
-		System.out.println("xixi");
+		System.out.println("xi1xi");
 		return menuServiceImpl.selAll() ;
 	}
 }
